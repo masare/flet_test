8c6223ef15b5692e48a8830d1d2503d5593d3084
masare <asaremark.ma@gmail.com>
2026-02-14 22:16:46 +0000

    Added a user name dialog. This made it possible to tag a message with the sender

--- chat_app/src/main.py
+++ chat_app/src/main.py
@@ -6,23 +6,54 @@ import flet as ft
 class Message:
     user: str
     text: str
+    message_type: str
 
 def main(page: ft.Page):
     page.title = ft.Text('Chat App')
     chat = ft.Column()
     new_message = ft.TextField()
+    user_name = ft.TextField(label="Enter your name")
+
 
     def on_message(message: Message):
-        chat.controls.append(ft.Text(f"{message.user}: {message.text}"))
+        if message.message_type == "chat message":
+            chat.controls.append(ft.Text(f"{message.user}: {message.text}"))
+        elif message.message_type == "login message":
+            chat.controls.append(
+                ft.Text(message.text, italic=True, color=ft.Colors.BLACK_45, size=12)
+            )
         page.update()
 
     page.pubsub.subscribe(on_message)
 
     def send_click(e):
-        page.pubsub.send_all(Message(user=page.session.id, text=new_message.value))
+        page.pubsub.send_all(Message(user=page.session.store.get("user_name"), text=new_message.value, message_type="chat message"))
         # chat.controls.append(ft.Text(new_message.value))
         new_message.value = ""
     
+    def join_chat(e):
+        if not user_name.value:
+            user_name.error = "Name cannot be blank"
+            user_name.update()
+        else:
+            page.session.store.set("user_name", user_name.value)
+            page.pop_dialog()
+            page.pubsub.send_all(Message(user=user_name.value, text=f"{user_name.value} has joined the chat", message_type="login message"))
+        page.update()
+
+    page.show_dialog(
+        ft.AlertDialog(
+            open=True,
+            modal=True,
+            title=ft.Text("Welcome!"),
+            content=ft.Column([user_name], tight=True),
+            actions=[
+                ft.Button(content='Join chat', on_click=join_chat)
+            ],
+            actions_alignment=ft.MainAxisAlignment.END,
+        )
+    )
+
     page.add(
         chat,
         ft.Row(
