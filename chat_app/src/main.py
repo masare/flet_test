adb273f4c6d676477e56fa0a5ef64ec41a85d2cf
masare <asaremark.ma@gmail.com>
2026-02-14 20:43:08 +0000

    Added a message broadcast functionality using PubSub

--- chat_app/src/main.py
+++ chat_app/src/main.py
@@ -1,13 +1,26 @@
+from dataclasses import dataclass
+
 import flet as ft
 
+@dataclass
+class Message:
+    user: str
+    text: str
 
 def main(page: ft.Page):
     page.title = ft.Text('Chat App')
     chat = ft.Column()
     new_message = ft.TextField()
 
+    def on_message(message: Message):
+        chat.controls.append(ft.Text(f"{message.user}: {message.text}"))
+        page.update()
+
+    page.pubsub.subscribe(on_message)
+
     def send_click(e):
-        chat.controls.append(ft.Text(new_message.value))
+        page.pubsub.send_all(Message(user=page.session.id, text=new_message.value))
+        # chat.controls.append(ft.Text(new_message.value))
         new_message.value = ""
     
     page.add(
